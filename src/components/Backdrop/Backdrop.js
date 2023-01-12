ecfa17f7f1a75b8d4b188d0a32cc5556e29dd25d
joker69216 <hamaraad883@gmail.com>
2023-01-12 15:59:42 +0300

    add animation to the modal

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Transition } from 'react-transition-group';
 
 import './Backdrop.css';
 
 const backdrop = (props) => {
-  const cssClasses = [
-    'Backdrop',
-    props.show ? 'BackdropOpen' : 'BackdropClose',
-  ];
-  return <div className={cssClasses.join(' ')}></div>;
+  return (
+    <Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
+          {(state) => {
+        const cssClasses = [
+          'Backdrop',
+          state === 'entering'
+            ? 'BackdropOpen'
+            : state === 'exiting'
+            ? 'BackdropClose'
+            : null,
+        ];
+        return <div className={cssClasses.join(' ')}></div>;
+      }}
+    </Transition>
+  );
 };
 
 export default backdrop;
