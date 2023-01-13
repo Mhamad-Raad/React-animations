b5a1b4f9e9d2b3f1602a4f894d6c9304de6d6758
joker69216 <hamaraad883@gmail.com>
2023-01-13 23:31:20 +0300

    change transition to CSSTransition object

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -1,23 +1,39 @@
 import React from 'react';
-import { Transition } from 'react-transition-group';
+// import { Transition } from 'react-transition-group';
+import { CSSTransition } from 'react-transition-group';
 
 import './Backdrop.css';
 
 const backdrop = (props) => {
   return (
-    <Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
-          {(state) => {
-        const cssClasses = [
-          'Backdrop',
-          state === 'entering'
-            ? 'BackdropOpen'
-            : state === 'exiting'
-            ? 'BackdropClose'
-            : null,
-        ];
-        return <div className={cssClasses.join(' ')}></div>;
+    // <Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
+    //       {(state) => {
+    //     const cssClasses = [
+    //       'Backdrop',
+    //       state === 'entering'
+    //         ? 'BackdropOpen'
+    //         : state === 'exiting'
+    //         ? 'BackdropClose'
+    //         : null,
+    //     ];
+    //     return <div className={cssClasses.join(' ')}></div>;
+    //   }}
+    // </Transition>
+
+    <CSSTransition
+      in={props.show}
+      timeout={300}
+      mountOnEnter
+      unmountOnExit
+      classNames={{
+        enter: '',
+        enterActive: 'BackdropOpen',
+        exit: '',
+        exitActive: 'BackdropClose',
       }}
-    </Transition>
+    >
+      <div className='Backdrop'></div>
+    </CSSTransition>
   );
 };
 
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,30 +1,51 @@
 import React from 'react';
-import { Transition } from 'react-transition-group';
+// import { Transition } from 'react-transition-group';
+import { CSSTransition } from 'react-transition-group';
 
 import './Modal.css';
 
 const modal = (props) => {
   return (
-    <Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
-      {(state) => {
-        const cssClasses = [
-          'Modal',
-          state === 'entering'
-            ? 'ModalOpen'
-            : state === 'exiting'
-            ? 'ModalClosed'
-            : null,
-        ];
-        return (
-          <div className={cssClasses.join(' ')}>
-            <h1>A Modal</h1>
-            <button className='Button' onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
+    // <Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
+    //   {(state) => {
+    //     const cssClasses = [
+    //       'Modal',
+    //       state === 'entering'
+    //         ? 'ModalOpen'
+    //         : state === 'exiting'
+    //         ? 'ModalClosed'
+    //         : null,
+    //     ];
+    //     return (
+    //       <div className={cssClasses.join(' ')}>
+    //         <h1>A Modal</h1>
+    //         <button className='Button' onClick={props.closed}>
+    //           Dismiss
+    //         </button>
+    //       </div>
+    //     );
+    //   }}
+    // </Transition>
+
+    <CSSTransition
+      in={props.show}
+      timeout={300}
+      mountOnEnter
+      unmountOnExit
+      classNames={{
+        enter: '',
+        enterActive: 'ModalOpen',
+        exit: '',
+        exitActive: 'ModalClosed',
       }}
-    </Transition>
+    >
+      <div className='Modal'>
+        <h1>A Modal</h1>
+        <button className='Button' onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
